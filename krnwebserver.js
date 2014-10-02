bb96f9fbde607970f42ca3e8bc3226466d461879
Rafael Quirino <quirinobytes@gmail.com>
2014-10-02 03:37:05 -0300

    novas modificações para manter a ultima versao

--- krnwebserver.js
+++ krnwebserver.js
@@ -5,28 +5,30 @@ var fs = require('fs');
 console.log("Quirino's Web Server started");
 
 http.createServer(function (req, res) {
-  
-  	//res.put(req.hearders);
-    console.log(console.time() + "A: " + req.headers.host + " | " + req.method + " | " + req.url);
-    
-	if (req.url == "/favicon.ico"){
-			fs.readFile('favicon.ico',function(err, data){
-				res.writeHead(200, {'Content-Type': 'text/plain'});
-				res.end(data);
-				console.log("FAVORITE ICON now!");
-				//console.log(data);
-			});
 
-	}
+        //res.put(req.hearders);
+    console.log(console.time() + "A: " + req.headers.host + " | " + req.method +
+ " | " + req.url);
 
-	if (req.url == "/")
-			fs.readFile('index.html',function(err, data){
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			//res.emit("sds");
-			res.end(data);
-		});
- // res.end('JÃ¡ terminei o programa, so estou esperando ler o arquivo\n');
- 
+        if (req.url == "/favicon.ico"){
+                        fs.readFile('favicon.ico',function(err, data){
+                                res.writeHead(200, {'Content-Type': 'text/plain'
+});
+                                res.end(data);
+                                console.log("FAVORITE ICON now!");
+                                //console.log(data);
+                        });
+
+        }
+
+        if (req.url == "/")
+                        fs.readFile('index.html',function(err, data){
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        //res.emit("sds");
+                        res.end(data);
+                });
+ // res.end('Já terminei o programa, so estou esperando ler o arquivo\n');
+
 }).listen(80, '0.0.0.0');
 
 console.log('Server running at http://127.0.0.1:80/');
